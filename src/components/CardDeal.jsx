b40709a461e5f8f26bb97e5dd18404bc2dd9a0c3
Roan Jagunap <89830405+Raiajgnp@users.noreply.github.com>
2025-01-25 10:41:32 +0800

    Add files via upload

---
+++ src/components/CardDeal.jsx
@@ -0,0 +1,27 @@
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
+
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className="sm:block hidden" /> in few easy
+        steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+      Hoobank makes it effortless to discover the best credit card deals. With just a few simple steps, 
+      you’ll find options that align with your financial goals, whether it’s earning rewards, saving on fees, 
+      or building credit.
+      </p>
+
+      <Button styles={`mt-10`} />
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="billing" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+);
+
+export default CardDeal;
